e4bda0315aaff35286ac842e71a80e00e95a42a5
jimidi <jimi@yahoo.co.uk>
2020-01-02 11:29:46 +0100

    changes to url

--- src/appsettings.js
+++ src/appsettings.js
@@ -1,5 +1,5 @@
 const Settings =  {
-    "baseUrl" : "http://localhost:8888/GitHub/middlewares90/api/",
+    "baseUrl" : "http://mware.social90.com/api/",
     "token" : "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpZCI6IjIxNDFjM2IxNzc5YTY0OWJlNjVkNDYxMWQ4NDVjNjU3MjEyZTRjODMiLCJqdGkiOiIyMTQxYzNiMTc3OWE2NDliZTY1ZDQ2MTFkODQ1YzY1NzIxMmU0YzgzIiwiaXNzIjoiIiwiYXVkIjoiQ0xJRU5UX0lEIiwic3ViIjpudWxsLCJleHAiOjE1NzUyMDQ5MzYsImlhdCI6MTU3NTIwMTMzNiwidG9rZW5fdHlwZSI6ImJlYXJlciIsInNjb3BlIjpudWxsfQ.qM0uTZMebRVgiYmUf8yXYc5EyX1WNopNeHfw6-7_hXYnkqJLWEG7i_F7ts-NRg1OERMrQgbv2REa1wwuPYY_xOAVmCOprPUzor95ynm0MYgY2zcCBIi0pe0b-FiOZlsys3zsgQ9tnm8fBqS_ZN9bmqwLfLS3mlLv-CF-1XzwqKE",
     "endPoints" : {
         "lApiUrl" : "postFtn/likeToggle/",
